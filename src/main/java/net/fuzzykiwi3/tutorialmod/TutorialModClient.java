bde1fb1d72b1e4af3a2d0e6b4de44b46ce1d1b2f
FuzzyKiwi3 <collin.greco@gmail.com>
2025-08-07 16:48:44 -0400

    Initial commit

---
+++ src/main/java/net/fuzzykiwi3/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.fuzzykiwi3.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
